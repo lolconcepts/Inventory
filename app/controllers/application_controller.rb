d73b3b3ada4bff3eca7f11e811c5bedd64f75e02
Bryan Stewart <bstewart@lolconcepts.com>
2019-10-17 07:42:28 -0400

    added basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+http_basic_authenticate_with name: ENV["USER_NAME"],
+                               password: ENV["PASSWORD"],
+                               if: -> { ENV["PASSWORD"].present? }
 end
